6f7889db48421614bf640754cf6187e17865add3
sjp46 <sjp46@njit.edu>
2017-04-25 15:29:04 -0400

    chnaged files to match teachers notes online

--- database.php
+++ database.php
@@ -9,35 +9,7 @@
 	$password = '6IX59hy8y';
 	$database = 'sjp46';
 	
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$number = $_POST['number'];
-	$birthday = $_POST['birthday'];
-	$gender = $_POST['gender'];
 
-	//create connection
-	$conn = new mysqli(sql1.njit.edu, sjp46, 6IX59hy8y, sjp46);
-
-	//check connection
-	if ($conn->connection_error){
-		die("Connection Failed: " . $conn->connect_error);
-	}
-	$sql = "INSERT INTO Final Project (First Name, Last Name, Email (Login), Phone Number, Birthday, Gender)
-	VALUES ('$fname, $lname, $email, $password, $number, $birthday, $gender')";
-
-	if ($conn->query($sql) === TRUE) {
-		echo "Thank you";
-	}
-	else {
-		echo "error: " .$sql . "<br>" . $conn->error;
-	}
-	
-	$conn-close();
-
-	try {
-        
         //$db = new PDO($dsn, $username, $password);
         $db = new PDO("mysql:host=mysql1.njit.edu;dbname=$database", $username, $password);
         
