e3b0b81d299166f5de6c204d45bd84b944d54296
sjp46 <sjp46@njit.edu>
2017-04-25 11:43:52 -0400

    Created the connection file to the php database and called it Database.php

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+	
+	date_default_timezone_set('America/New_York');
+	
+	
+	
+	$hostname = 'sql1.njit.edu';
+	$username = 'sjp46';
+	$password = '6IX59hy8y';
+	$database = 'sjp46';
+	
+	
+	
+	try {
+        
+        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO("mysql:host=mysql1.njit.edu;dbname=$database", $username, $password);
+        
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
