ca67978ef17841dbf0648c980450baa9ee2255b3
sjp46 <sjp46@njit.edu>
2017-04-25 14:11:58 -0400

    Added a connection to the php database! but doesnt work :C

--- database.php
+++ database.php
@@ -9,7 +9,32 @@
 	$password = '6IX59hy8y';
 	$database = 'sjp46';
 	
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+	$birthday = $_POST['birthday'];
+	$gender = $_POST['gender'];
+
+	//create connection
+	$conn = new mysqli($hostname, $username, $password, $database);
+
+	//check connection
+	if ($conn->connection_error){
+		die("Connection Failed: " . $conn->connect_error);
+	}
+	$sql = "INSERT INTO Final Project (First Name, Last Name, Email (Login), Phone Number, Birthday, Gender)
+	VALUES ('$fname, $lname, $email, $password, $number, $birthday, $gender')";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Thank you";
+	}
+	else {
+		echo "error: " .$sql . "<br>" . $conn->error;
+	}
 	
+	$conn-close();
 	
 	try {
         
