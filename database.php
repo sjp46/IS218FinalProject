d95c7efc73808320f3cbbdf5ff8299d0d8f63923
sjp46 <sjp46@njit.edu>
2017-04-25 11:56:20 -0400

    Changed the file type of the linked database_error file from php to html

--- database.php
+++ database.php
@@ -18,7 +18,7 @@
         
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
-        include('database_error.php');
+        include('database_error.html');
         exit();
     }
 ?>
