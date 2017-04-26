912e9d0937d13f8bcf758841544fd16da365aa05
sjp46 <sjp46@njit.edu>
2017-04-26 17:28:52 -0400

    vedv

--- PDOconnect.php
+++ PDOconnect.php
@@ -5,13 +5,13 @@ require('database.php');
 $fname=$_POST['fname'];
 $lname=$_POST['lname'];
 $email=$_POST['email'];
-$phone=$_POST['phone'];
+$number=$_POST['number'];
 $birthday=$_POST['birthday'];
 $gender=$_POST['gender'];
 $password=$_POST['password'];
 
-$sql="INSERT INTO acc (fname,lname,email,phone,birthday,gender,password) VALUES (
-'$fname','$lname','$email','$phone','$birthday','$gender','$password')";
+$sql="INSERT INTO acc (fname,lname,email,number,birthday,gender,password) VALUES (
+'$fname','$lname','$email','$number','$birthday','$gender','$password')";
 
 
 $statement = $db->prepare($sql);
